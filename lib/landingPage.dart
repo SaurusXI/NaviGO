26ce959edc79ba3df090035934bdb2c8e6180d19
SamSan123 <sanyamagrawal3@gmail.com>
2020-02-28 14:36:54 +0530

    Added Camera Module

--- lib/landingPage.dart
+++ lib/landingPage.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
 
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+            
     );
   }
 }
